171eb592552dde139e0f92419f16ef98b86b7de0
Daniel António Pinto Ferreira <danielf18@hotmail.com>
2021-07-09 16:54:27 +0100

    delete setup.sh
    stuff that does not need to be here

--- backend/setup.sh
+++
@@ -1,2 +0,0 @@
-export DATABASE_URL='postgresql://vbbqqgzsmxvhdk:b601920567f87b51640cbc8bee7b5e3a592e955873e5ba956d630333b4da9689@ec2-34-193-113-223.compute-1.amazonaws.com:5432/dcb8rkk0nhhun4'
-export TEST_DATABASE_URL='postgres://ucdigpkmytvdfx:3bb4db5eaaf5c3db00ec66b955d39b61ede98115f61e140ea5788876afe5c8e0@ec2-52-0-114-209.compute-1.amazonaws.com:5432/d8h5n1rv5vs4df'
